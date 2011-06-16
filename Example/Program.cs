5012846e6f39415e294842000610117f6b9f2702
Clyde Balneaves <tagusk@hotmail.com>
2011-06-16 07:56:51 +0100

    Test app

---
+++ Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient("", 8125);
+
+            string test = "system.glork:320|ms";
+
+            Random rnd = new Random();
+            while (true)
+            {
+                client.Send(Encoding.ASCII.GetBytes(test), Encoding.ASCII.GetByteCount(test));
+                Thread.Sleep(rnd.Next(500, 3000));
+            }
+
+            client.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
