b04d5ce18e22a1a2ccd17bcb0fd08ab86c8dfbb7
Clyde Balneaves <tagusk@hotmail.com>
2011-06-17 10:02:36 +0100

    Learning graphite... didn't need rolling up before posting

--- GraphitePipe.net/Key.cs
+++ GraphitePipe.net/Key.cs
@@ -7,24 +7,14 @@ namespace Rbi.Monitoring.Graphite
         public const string Root = "stat";
         public const string RollupRoot = "roll";
 
-        public string Vertical { get; set; }
+        public string Product { get; set; }
         public string DataCentre { get; set; }
         public string Server { get; set; }
         public string Stat { get; set; }
 
         public string FullKeyName
         {
-            get { return  String.Format("{0}.{1}.{2}.{3}.{4}", Root, Vertical, DataCentre, Server, Stat); }
-        }
-
-        public string DataCentreKeyName
-        {
-            get { return String.Format("{0}.{1}.{2}.{3}.{4}", Root, RollupRoot, Vertical, DataCentre, Stat); }
-        }
-
-        public string VerticalKeyName
-        {
-            get { return String.Format("{0}.{1}.{2}.{3}", Root, RollupRoot, Vertical, Stat); }
+            get { return String.Format("{0}.{1}.{2}.{3}.{4}", Root, Product, DataCentre, Server, Stat); }
         }
     }
 }
